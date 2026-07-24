f9918e3a4352beff6b2c0ca874c5cbb79090a2be
UIMSolutions <ui.manufaktur@gmail.com>
2026-07-24 16:41:48 +0200

    Init

---
+++ source/app.d
@@ -0,0 +1,26 @@
+import vibe.http.server;
+import vibe.http.router;
+import vibe.core.core : runApplication;
+import std.process : environment;
+import std.conv : to;
+
+void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
+{
+    res.writeBody("Hallo von Dlang auf der SAP BTP Cloud Foundry!", "text/plain; charset=UTF-8");
+}
+
+void main()
+{
+    auto settings = new HTTPServerSettings;
+    
+    // Cloud Foundry Port dynamisch auslesen
+    ushort port = environment.get("PORT", "8080").to!ushort;
+    settings.port = port;
+    settings.bindAddresses = ["0.0.0.0"];
+
+    auto router = new URLRouter;
+    router.get("/", &handleRequest);
+
+    listenHTTP(settings, router);
+    runApplication();
+}
